b202d127cd00a80182f6c7bc7d12f88f1fb15f08
机票事业群-严锐 <yr49392@tcent.cn>
2019-04-17 16:26:23 +0800

    finish

--- src/components/form/form.vue
+++ src/components/form/form.vue
@@ -15,6 +15,11 @@ export default {
             type: Object
         }
     },
+    provide() {
+        return {
+            form: this
+        };
+    },
     data() {
         return {
             fields: []
@@ -32,7 +37,34 @@ export default {
             }
         });
     },
-    methods: {}
+    methods: {
+        // 公开方法：全部重置数据
+        resetFields() {
+            this.fields.forEach(field => {
+                field.resetField();
+            });
+        },
+        validate(callback) {
+            return new Promise(resolve => {
+                let valid = true;
+                let count = 0;
+                this.fields.forEach(field => {
+                    field.validate("", errors => {
+                        if (errors) {
+                            valid = false;
+                        }
+                        if (++count === this.fields.length) {
+                            // 全部完成
+                            resolve(valid);
+                            if (typeof callback === "function") {
+                                callback(valid);
+                            }
+                        }
+                    });
+                });
+            });
+        }
+    }
 };
 </script>
 
--- src/views/form.vue
+++ src/views/form.vue
@@ -1,7 +1,8 @@
+    
 <template>
     <div>
-        <h3>具有数据校验功能的表单组件</h3>
-        <i-form :model="formValidate" :rules="ruleValidate">
+        <h3>具有数据校验功能的表单组件——Form</h3>
+        <i-form ref="form" :model="formValidate" :rules="ruleValidate">
             <i-form-item label="用户名" prop="name">
                 <i-input v-model="formValidate.name"></i-input>
             </i-form-item>
@@ -13,12 +14,12 @@
         <button @click="handleReset">重置</button>
     </div>
 </template>
-
 <script>
 import iForm from "../components/form/form.vue";
 import iFormItem from "../components/form/form-item.vue";
 import iInput from "../components/input/input.vue";
 export default {
+    components: { iForm, iFormItem, iInput },
     data() {
         return {
             formValidate: {
@@ -49,14 +50,18 @@ export default {
         };
     },
     methods: {
-        handleSubmit() {},
-        handleReset() {}
-    },
-    components: {
-        iForm,
-        iFormItem,
-        iInput
+        handleSubmit() {
+            this.$refs.form.validate(valid => {
+                if (valid) {
+                    window.alert("提交成功");
+                } else {
+                    window.alert("表单校验失败");
+                }
+            });
+        },
+        handleReset() {
+            this.$refs.form.resetFields();
+        }
     }
 };
 </script>
-
